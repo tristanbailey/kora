abfcab90a2d9583b12070a778bb07d46ac423cce
James Lambie <jameslambie@gmail.com>
2013-08-21 09:39:58 +0100

    New currency rounding package

---
+++ app/code/local/Kora/Rounding/Model/Currency.php
@@ -0,0 +1,24 @@
+/*
+http://www.tall-paul.co.uk/2012/03/28/magento-round-up-currency-conversions/
+*/
+
+class Kora_Rounding_Model_Currency extends Mage_Directory_Model_Currency {
+ /**
+ * Convert price to currency format
+ *
+ * @param   double $price
+ * @param   string $toCurrency
+ * @return  double
+ */
+
+ public function convert($price, $toCurrency=null)
+ {
+    if (is_null($toCurrency)) {
+      return $price;
+    }
+    elseif ($rate = $this->getRate($toCurrency)) {
+       return ceil($price*$rate);
+    }
+    throw new Exception(Mage::helper('directory')->__('Undefined rate from "%s-%s".', $this->getCode(), $toCurrency->getCode()));
+ }
+}
